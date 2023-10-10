22f4c46c3525bd6330e3f0b4570e2c0e5641a18f
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-10 16:34:57 -0700

    w

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int n)
 	int last_d;
 	char r;
 
+	last_d = n % 10;
 	if (n < 0)
 	{
-		n *= -1;
+		last_d *= -1;
 	}
-	last_d = n % 10;
 	r = last_d + '0';
 	_putchar(r);
 	return (last_d);

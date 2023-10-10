fb477221c1171698f1e02bc53b51d2edd6edd56c
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-10 16:18:15 -0700

    w

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value
+ * @n: The number of operation
+ *
+ * Return: The absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n - (n * 2));
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,8 @@ int _putchar(char c);
 
 void print_alphabet(void);
 
+int _abs(int);
+
 void print_alphabet_x10(void);
 
 int _islower(int c);

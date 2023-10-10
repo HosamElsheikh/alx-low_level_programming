b9bb1ef73e447d1702c11baec6160611023e88a1
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-10 15:34:17 -0700

    w

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all the alphabet
+ *
+ * in lower case
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = "a", c < 123, c++)
+	{
+		_putchar("%c", c);
+	}
+}

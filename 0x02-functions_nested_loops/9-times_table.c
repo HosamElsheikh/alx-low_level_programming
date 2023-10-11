9e7f4e07aaea359709bea2b3088c27dab525b6cd
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-10 17:06:52 -0700

    gfsd

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,16 +18,20 @@ void times_table(void)
 			result = i * j;
 			if (result > 9)
 			{
+				_putchar(' ');
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
 			}
 			else
 			{
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(result + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+			if (i != 9)
+			{
+				_putchar(',');
+			}
 		}
 		_putchar('\n');
 	}

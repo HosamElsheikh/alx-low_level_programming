a33d825722eed2fe8088f038967f6d8d1f0d448d
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-10 17:23:11 -0700

    w

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,37 +7,29 @@
  */
 void times_table(void)
 {
-	int i;
-	int j;
+	int row;
+	int col;
 	int result;
 
-	for (i = 0; i < 10; i++)
+	for (row = 0; row < 10; row++)
 	{
-		for (j = 0; j < 10; j++)
+		for (col = 0; col < 10; col++)
 		{
-			result = i * j;
-			if (i == 0)
+			result = row * col;
+			if (col == 0)
 			{
-				_putchar(result + '0');
+				_putchar('0');
 			}
 			else
 			{
-				if (result > 9)
-				{
-					_putchar(' ');
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-			}
-			if (j != 9)
-			{
 				_putchar(',');
+				_putchar(' ');
+				if (result < 10)
+				{
+					_putchar(' ');
+				}
 			}
+			_putchar(result + '0');
 		}
 		_putchar('\n');
 	}

2192b324d9f26fa02dd9c91e75d6e0a1b6c05f93
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-10 17:35:04 -0700

    w

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,8 +25,15 @@ void times_table(void)
 					_putchar(' ');
 				}
 			}
-			_putchar(result / 10 + '0');
-			_putchar(result % 10 + '0');
+			if (result > 9)
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else
+			{
+				_putchar(result + '0');
+			}
 		}
 		_putchar('\n');
 	}

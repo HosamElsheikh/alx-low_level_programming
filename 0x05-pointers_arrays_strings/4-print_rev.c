5917e6802c144b15148578381c91cfa63b3c7a7d
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-16 15:36:05 -0700

    da

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse
+ * @s: The string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	for (; *s != '\0'; s++)
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(*s);
+		s--;
+		i--;
+	}
+	_putchar('\n');
+}

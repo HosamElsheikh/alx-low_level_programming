3ccea19bb20e8ac94ba70ca48559234bf21fef6f
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-16 16:46:53 -0700

    ds

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: The input string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length;
+	int i, start;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		start = length / 2;
+	}
+	else
+	{
+		start = (length - 1) / 2;
+	}
+
+	for (i = start; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

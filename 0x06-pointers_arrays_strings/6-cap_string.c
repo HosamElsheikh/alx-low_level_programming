377903994f2682027211c55d35c5217186b0937c
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-18 15:59:24 -0700

    s

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize all words of a string.
+ * @str: The input string.
+ *
+ * Return: A pointer to the capitalized string.
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') &&
+		(i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+		str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
+		str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
+		str[i - 1] == '{' || str[i - 1] == '}'))
+		{
+			str[i] -= 32;
+		}
+	}
+
+	return str;
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 void reverse_array(int *a, int n);
-
+char *cap_string(char *);
 char *string_toupper(char *);
 int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);

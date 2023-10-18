898809ee8626ff1aaa70987be83883f9e71b1d52
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-18 16:03:21 -0700

    s

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,13 +14,14 @@ char *cap_string(char *str)
 	{
 		if ((str[i] >= 'a' && str[i] <= 'z') &&
 		(i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-		str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
-		str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' ||
-		str[i - 1] == '{' || str[i - 1] == '}'))
+		str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+		str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+		str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+		str[i - 1] == '}'))
 		{
 			str[i] -= 32;
 		}
 	}
 
-	return str;
+	return (str);
 }

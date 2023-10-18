e992d7f12f9dc3618c6d9d82a9bb842a1cda15fd
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-18 15:42:43 -0700

    s

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 void reverse_array(int *a, int n);
 
-
+char *string_toupper(char *);
 int _strcmp(char *s1, char *s2);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);

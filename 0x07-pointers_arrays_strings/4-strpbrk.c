bc7dc3a7d949f3ccb25c7ecb1e4d66c1e53e1152
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-23 15:41:52 -0700

    use my braing

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,8 @@
  * @s: The string to search.
  * @accept: The string containing characters to search for.
  *
- * Return: A pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found.
+ * Return: A pointer to the byte in s that matches
+ * one of the bytes in accept, or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {

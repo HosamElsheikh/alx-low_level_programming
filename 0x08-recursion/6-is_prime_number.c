22faa10d2c4ae24a5a5b4dc93e3ee635bfec4c4c
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-25 16:09:27 -0700

    sasa

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,24 @@
 /**
+ * check_prime - Helper function to check if n is divisible by any number from start.
+ * @n: The number to check for primality.
+ * @k: The starting number to check divisibility.
+ *
+ * Return: 1 if prime, 0 if not.
+ */
+int check_prime(int n, int k)
+{
+	if ((k * k) > n)
+	{
+		return (1);
+	}
+	if (n % k == 0)
+	{
+		return (0);
+	}
+	return (check_prime(n, k + 1));
+}
+
+/**
  * is_prime_number - Check if a number is prime.
  * @n: The number to check.
  *
@@ -8,21 +28,5 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (check_prime(n,2));
-}
-
-/**
- * check_prime - Helper function to check if n is divisible by any number from start.
- * @n: The number to check for primality.
- * @k: The starting number to check divisibility.
- *
- * Return: 1 if prime, 0 if not.
- */
-int check_prime(int n; int k)
-{
-	if (k * k > m)
-		return (1);
-	if (n % k == 0)
-		return (0);
-	return (check_prime(n; k + 1))
+	return (check_prime(n, 2));
 }

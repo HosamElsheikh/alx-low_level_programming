af6314cd2dc81a24fdb0461000645fff4dea44f2
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-25 14:08:09 -0700

    fix betty

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,9 +12,7 @@ int factorial(int n)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	else
-	{
-		fac = n * factorial(n - 1);
-		return (fac);
-	}
+
+	fac = n * factorial(n - 1);
+	return (fac);
 }

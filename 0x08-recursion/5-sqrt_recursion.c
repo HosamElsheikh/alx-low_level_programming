ffb0484c1029033fbd80a69e3cf587c65d5632df
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-25 15:07:23 -0700

    cheat

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,14 +8,15 @@
 int _sqrt_helper(int n, int guess)
 {
 	if (guess * guess == n)
-		return guess;
+		return (guess);
 	else if (guess * guess > n)
-		return -1;
-	return _sqrt_helper(n, guess + 1);
+		return (-1);
+	return (_sqrt_helper(n, guess + 1));
 }
 
 /**
- * _sqrt_recursion - Calculate the natural square root of a number using recursion
+ * _sqrt_recursion - Calculate the natural
+ * square root of a number using recursion
  * @n: The number for which to calculate the square root
  *
  * Return: The natural square root if found, -1 if not found.
@@ -23,6 +24,6 @@ int _sqrt_helper(int n, int guess)
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1;
-	return _sqrt_helper(n, 0);
+		return (-1);
+	return (_sqrt_helper(n, 0));
 }

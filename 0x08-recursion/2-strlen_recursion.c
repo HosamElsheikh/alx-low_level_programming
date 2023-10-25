e37564664615426bb9e6d16486fe67436c4d1d37
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-10-25 13:47:25 -0700

    stupid

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen_recursion - Returns the length of a string using recursion
+ * @s: The string to find the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s == '\0')
+		return (count);
+	return 1 + _strlen_recursion(s + 1);
+}

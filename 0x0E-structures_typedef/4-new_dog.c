80f01e8bada7b6b1b53d3b73c8b77d650c12dbef
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-11-06 11:32:54 -0800

    s

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,26 +14,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int name_len, owner_len, i;
 	char *new_name, *new_owner;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_name = strdup(name);
-	if (new_name == NULL)
+	name_len = 0;
+	owner_len = 0;
+	if (name)
 	{
-		free(new_dog);
-		return (NULL);
+		while (name[name_len])
+			name_len++;
 	}
-	new_owner = strdup(owner);
-	if (new_owner == NULL)
+	if (owner)
+	{
+		while (owner[owner_len])
+			owner_len++;
+	}
+	new_name = malloc(name_len + 1);
+	new_owner = malloc(owner_len + 1);
+	if (!new_name || !new_owner)
 	{
 		free(new_name);
+		free(new_owner);
 		free(new_dog);
 		return (NULL);
 	}
+	for (i = 0; i <= name_len; i++)
+		new_name[i] = name[i];
+	for (i = 0; i <= owner_len; i++)
+		new_owner[i] = owner[i];
 	new_dog->name = new_name;
 	new_dog->age = age;
 	new_dog->owner = new_owner;
+
 	return (new_dog);
 }

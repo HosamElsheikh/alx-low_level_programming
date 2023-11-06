2d9a724f20ef9a4c38c9b1562facb5b1ddded052
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-11-06 10:53:12 -0800

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the name of the source file.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf(__FILE__ "\n");
+	return (0);
+}

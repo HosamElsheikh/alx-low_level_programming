581581366508e171b988218926705911176f463c
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-11-16 16:26:53 -0800

    fuck

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: A pointer to the head of the linked list.
- * 
+ *
  * Return: The number of nodes.
  */
 size_t print_list(const list_t *h)

8acb9efaf25a879edf0a877bdd0cbb299dd33219
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-11-20 14:31:03 -0800

    fixed the arrangement of the code

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,12 +13,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *newNode;
 
 	newNode = malloc(sizeof(listint_t));
-	newNode->next = *head;
-	*head = newNode;
-	newNode->n = n;
-
 	if (newNode == NULL)
 		return (NULL);
+
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
 
 	return (newNode);
 }

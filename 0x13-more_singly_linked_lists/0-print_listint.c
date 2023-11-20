6e5a0b364850f2aff6a19d3d35ce5f71a36db4de
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-11-20 13:10:34 -0800

    test

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * print_number - Prints an integer using _putchar.
+ * @n: The integer to print.
+ */
+void printNumber(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		printNumber(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
+/**
+ * print_listint - Prints the numbers
+ *
+ * @h: the pointer to the list
+ *
+ * Return: The size of the linked list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodeCount = 0;
+
+	while (h != NULL)
+	{
+		printNumber(h->n);
+		_putchar('\n');
+		h = h->next;
+		nodeCount++;
+	}
+	return (nodeCount);
+}

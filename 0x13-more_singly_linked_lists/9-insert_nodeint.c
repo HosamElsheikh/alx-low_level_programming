9f4719a50e655ab33633c63d185f4eaad2988200
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-11-21 14:26:34 -0800

    smart af

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include lists.h
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - inserting a new node at agiven postion
+ * @head: double pointer
+ * @idx: index of the node
+ * @n: new node value
+ *
+ * Return: the address of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newNode = malloc(sizeof(listint_t));
+	listint_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+
+	if (newNode == NULL)
+		return (NULL);
+
+	if (idx != 0)
+	{
+		temp = *head;
+		for (i = 0; i < idx - 1 && temp != NULL; i++)
+		{
+			temp = temp->next;
+		}
+		if (temp == NULL)
+			return (NULL);
+	}
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
+}

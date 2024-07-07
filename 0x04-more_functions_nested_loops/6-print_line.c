3ee42b6cd18f1969776ab7e1aa8a38837089e643
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-07-08 00:33:26 +0300

    WHAT IS WRONG

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,19 +8,16 @@
 
 void print_line(int n)
 {
-	int i;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
-	}
+		int i;
 
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
 }

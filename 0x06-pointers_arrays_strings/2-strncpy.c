9db6dcbedd06b5a53d6d9e39cefb2bb840d6d0ff
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-07-09 23:03:23 +0300

    Not bad

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,24 @@
+#include "main.h"
+
 /**
- * _strncpy - copies s2 to s1 by n
- * @dest: The first string
- * @src: The string to concat
- * @n: The number of bytes
- *
- * Return: Pointer
+ * _strncpy - copies a string into another one
+ * @dest: Main string
+ * @src: The other string
+ * @n: Number of bytes to copy
+ * Return: The copied string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int length;
+	int i;
 
-	for (length = 0; length < n && src[length] != '\0'; length++)
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[length] = src[length];
+		dest[i] = src[i];
 	}
-	for ( ; length < n; length++)
-		dest[length] = '\0';
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 	return (dest);
 }

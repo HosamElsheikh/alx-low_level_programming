88e5216ff23e0e072ada7a7aaf1752f2c496856f
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-07-17 21:22:20 +0300

    I knew the idea but I cheated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,58 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+#include <string.h>
 /**
- * is_positive_number - Check if a string is a positive number
- * @str: The string to check
- * Return: 1 if it's a positive number, 0 otherwise
- */
-int is_positive_number(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (!isdigit(str[i]))
-			return (0);
-		i++;
-	}
-
-	return (1);
-}
-
-/**
- * main - Entry point
- * Description: Adds positive numbers and prints the result
- * @argc: Argument count
- * @argv: Argument vector
- * Return: 0 for success, 1 for error
- */
+* main - entry point
+* @argc: size of argv
+* @argv: array
+* Return: 0
+**/
 int main(int argc, char *argv[])
 {
-	int result = 0;
-	int i;
+	int i, j, sum = 0;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
 	for (i = 1; i < argc; i++)
 	{
-		if (is_positive_number(argv[i]))
+		if (argv[i][0] == '-')
 		{
-			result += atoi(argv[i]);
+			for (j = 1; argv[i][j] != '\0'; j++)
+			{
+				if (isdigit(argv[i][j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 		}
 		else
 		{
-
-			printf("Error\n");
-			return (1);
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (isdigit(argv[i][j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 		}
+		sum += atoi(argv[i]);
 	}
-
-	printf("%d\n", result);
+	printf("%d\n", sum);
 	return (0);
 }

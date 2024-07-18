fdd63fc86f282be4b3b8ffbcfb12c16c4c4a1073
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-07-18 22:58:56 +0300

    Should be OK

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,40 @@
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * str_concat - Concatenates two strings
+ * str_concat - concatenates two strings
  * @s1: The first string
  * @s2: The second string
- *
- * Return: A pointer to the concatenated string, or NULL on failure
+ * Return: Pointer to the total array
  */
 char *str_concat(char *s1, char *s2)
 {
-	int size1;
-	int size2;
-	int i;
-	int j;
-	char *ar;
-	int k1;
-	int k2;
+	char *ret;
+	int i, size1 = 0, size2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 
-	size1 = 0;
-	for (i = 0; s1[i] != '\0'; i++)
+	while (s1[size1] != '\0')
+	{
 		size1++;
-
-	size2 = 0;
-	for (j = 0; s2[j] != '\0'; j++)
+	}
+		while (s2[size2] != '\0')
+	{
 		size2++;
+	}
 
-	ar = malloc((size1 + size2 + 1) * sizeof(char));
-	if (ar == 0)
+	ret = malloc((sizeof(char) * (size1 + size2 + 1)));
+
+	if (ret == NULL)
+	{
 		return (NULL);
-
-	for (k1 = 0; k1 < size1; k1++)
-	{
-		ar[k1] = s1[k1];
 	}
-
-	for (k2 = 0; k2 <= size2; k2++)
+	for (i = 0; i < size1; i++)
 	{
-		ar[k1] = s2[k2];
-		k1++;
+		ret[i] = s1[i];
 	}
-
-	return (ar);
+	for (i = 0; i <= size2; i++)
+	{
+		ret[size1] = s2[i];
+		size1++;
+	}
+	return (ret);
 }

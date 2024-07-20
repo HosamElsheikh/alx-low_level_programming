e94b17f9f1bfe0be82f00f320d6524eee5436f2f
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-07-20 15:15:07 +0300

    Check the malloc ya m3lm

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * malloc_checked - Returns a pointer to the allocated memory
- * @b: The number of bytes to assign
- *
- * Return: pointer to the allocated memory
+ * malloc_checked - allocates memory using malloc
+ * @b: Number of bytes
+ * Return: A pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {

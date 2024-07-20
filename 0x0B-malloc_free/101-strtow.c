6c3f70e2ab29dc93d164188fcaca3036825a6dd5
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-07-20 03:40:11 +0300

    daadsads

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,31 +1,65 @@
 #include "main.h"
 
+int is_space(char c) {
+    return (c == ' ' || c == '\t' || c == '\n'); // Add other space characters as needed
+}
+
+int count_words(char *str) {
+    int count = 0;
+    int in_word = 0;
+    
+    while (*str) {
+        if (is_space(*str)) {
+            in_word = 0;
+        } else if (!in_word) {
+            in_word = 1;
+            count++;
+        }
+        str++;
+    }
+    
+    return count;
+}
+
 char **strtow(char *str) {
-
-	if (str == NULL || str[0] == '\0') {
-		return (NULL);
-	}
-
-	int i, j = 0, k = 0, count = 0;
-
-	for (i = 0; str[i]; i++)
-		if (str[i] == ' ')
-			count++;
-
-	char **words = malloc((count + 2) * sizeof(char *));
-
-	if (words == NULL) {
-		return (NULL);
-	}
-
-	words[j] = &str[k];
-	for (i = 0; str[i]; i++) {
-		if (str[i] == ' ') {
-			str[i] = '\0';
-			j++;
-			words[j] = &str[i + 1];
-		}
-	}
-	words[j + 1] = NULL;
-
-	return (words);
+    if (str == NULL || *str == '\0')
+        return NULL;
+    
+    int num_words = count_words(str);
+    char **words = (char **)malloc((num_words + 1) * sizeof(char *));
+    if (words == NULL)
+        return NULL;
+    
+    int word_index = 0;
+    int word_start = 0;
+    int word_length = 0;
+    
+    while (str[word_start]) {
+        while (is_space(str[word_start]))
+            word_start++;
+        
+        word_length = 0;
+        while (str[word_start + word_length] && !is_space(str[word_start + word_length]))
+            word_length++;
+        
+        words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+        if (words[word_index] == NULL) {
+            // Clean up allocated memory
+            for (int i = 0; i < word_index; i++)
+                free(words[i]);
+            free(words);
+            return NULL;
+        }
+        
+        for (int i = 0; i < word_length; i++)
+            words[word_index][i] = str[word_start + i];
+        words[word_index][word_length] = '\0';
+        
+        word_index++;
+        word_start += word_length;
+    }
+    
+    words[word_index] = NULL; // Null-terminate the array of words
+    
+    return words;
+}

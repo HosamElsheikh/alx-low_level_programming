bf14fed864f2da72a0c1caac4793a53a29b55c26
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-07-26 19:20:24 +0300

    Added the new function

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdlib.h>
 /**
  * struct dog - a struct for a dog
  * @name: The name of the dog
@@ -13,5 +14,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif

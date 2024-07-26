05b3df747340c18287e30f8cdef8700151a14260
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-07-26 19:13:21 +0300

    Wrong parameter name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a struct for a dog
+ * @name: The name of the dog
+ * @age: The age
+ * @owner: The owner
+ */
 struct dog
 {
-	char *dog;
+	char *name;
 	float age;
 	char *owner;
 };

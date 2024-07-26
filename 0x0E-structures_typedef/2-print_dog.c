bbf37c1d2bff037d83acac8772629ba56caf3548
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-07-26 19:29:58 +0300

    Added new chars and changed the return in case of NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,15 +8,15 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
-		return (0);
+		return;
 	}
 	if (d->name == NULL)
 		printf("Name: (nil)");
 		else
-	printf("Name: %s", d->name);
-	printf("Age: %.6f", d->age);
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)");
 		else
-	printf("Owner: %s", d->owner);
+	printf("Owner: %s\n", d->owner);
 }

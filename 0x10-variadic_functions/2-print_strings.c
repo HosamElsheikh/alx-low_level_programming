0a0cea87652674bfb3ddbbe0d9baf3d94d39f51a
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-07-28 19:24:33 +0300

    A variable is needed to store the string since va_arg iterates by invocation

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,35 +1,31 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
- * print_strings - Prints strings followed by a new line.
- * @separator: The string to be printed between the strings.
- * @n: The number of strings passed to the function.
- * @...: The strings to be printed.
+ * print_strings - prints strings
+ * @separator: separator
+ * @n: number of ints
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
+	va_list ap;
 	unsigned int i;
-	const char *current_str;
+	char *current;
 
-	va_start(args, n);
-
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		current_str = va_arg(args, const char *);
-
-		if (current_str != NULL)
-			printf("%s", current_str);
+		current = va_arg(ap, char *);
+		if (current)
+		{
+			printf("%s", current);
+		}
 		else
+		{
 			printf("(nil)");
-
-		if (separator != NULL && i < n - 1)
+		}
+		if (separator && i != n - 1)
 			printf("%s", separator);
 	}
-
-	va_end(args);
-
 	printf("\n");
+	va_end(ap);
 }

c044bd8ebfae705af461a29700e18b083a39e0c6
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-08-12 00:05:08 +0300

    Doing it with a for loop

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,17 +12,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	while (i <= index)
+	for (i = 0; i < index; i++)
 	{
-		if (i == index)
-			return (head);
-		else
-		{
-			i++;
-			head = head->next;
-			if (head == NULL)
-				return (NULL);
-		}
+		if (!head)
+			return (NULL);
+
+		head = head->next;
 	}
 	return (head);
 }

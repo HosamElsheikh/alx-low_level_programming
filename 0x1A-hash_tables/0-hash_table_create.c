1b93eac7ccad8410cb31bca7093cc8060a8c52cd
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-14 15:21:27 +0300

    No need for size * the nodes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,9 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-
-
-	table = calloc(size, sizeof(hash_node_t));
+	table = calloc(1, sizeof(hash_node_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;

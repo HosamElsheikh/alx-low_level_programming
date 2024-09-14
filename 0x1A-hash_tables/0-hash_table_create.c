1ba8243134e51fdd25a9e731b17458e501e394be
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-14 15:22:05 +0300

    what was I thinking>!?!??!!?

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	table = calloc(1, sizeof(hash_node_t));
+	table = calloc(1, sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
